424c107b8e5052472b6bac433c7ede5c06729d47
Joyrudra Biswas <joyrudra@itobuz.com>
2023-12-13 18:24:11 +0530

    main : repaired tsconfig file

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,21 @@ import "reflect-metadata";
 import path from "node:path";
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import { expressMiddleware } from "@apollo/server/express4";
 import { buildSchema } from "type-graphql";
 import { RecipeResolver } from "./graphQl/recipe/recipe.resolver";
+import express from "express";
+import cors from "cors";
+import config from "./config/config";
+
+console.log(config.PORT);
 
 async function bootstrap() {
+  const app = express();
+
+  app.use(cors());
+  app.use(express.json());
+
   // Build TypeGraphQL executable schema
   const schema = await buildSchema({
     // Array of resolvers
@@ -17,11 +28,15 @@ async function bootstrap() {
   // Create GraphQL server
   const server = new ApolloServer({ schema });
 
-  // Start server
-  const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
+  await server.start();
+
+  app.use("/graphql", expressMiddleware(server));
+
+  app.listen(config.PORT || 4040, () => {
+    console.log(
+      `Server Running on http://localhost:${config.PORT || 4040}/graphql 🚥`
+    );
   });
-  console.log(`GraphQL server ready at ${url}`);
 }
 
 bootstrap().catch(console.error);
